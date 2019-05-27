53aee9387491be55531a791b848d559023f1ad68
Mohammed Kuthari <iiiwea120@gmail.com>
2019-05-28 00:35:35 +0300

    Switch to local database

--- includes/config.php
+++ includes/config.php
@@ -6,19 +6,20 @@ session_start();
 date_default_timezone_set('Europe/London');
 
 //database credentials
+/*
 define('DBHOST','localhost');
 define('DBUSER','database username');
 define('DBPASS','password');
 define('DBNAME','database name');
-
+*/
 //application address
-define('DIR','http://domain.com/');
-define('SITEEMAIL','noreply@domain.com');
+define('DIR','monymanager.herokuapp.com');
+define('SITEEMAIL','noreply@monymanager.herokuapp.com');
 
 try {
 
 	//create PDO connection
-	$db = new PDO("mysql:host=".DBHOST.";charset=utf8mb4;dbname=".DBNAME, DBUSER, DBPASS);
+	$db = new PDO("sqlite:tmp/data.php");
     //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);//Suggested to uncomment on production websites
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Suggested to comment on production websites
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
